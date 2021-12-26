c0d418f5f371db0ccaf12f3891e51e1a69314368
Taranjeet Ghotra <ghotra452@gmail.com>
2021-12-26 20:39:31 +0100

    Anmeldung

---
+++ candy shop-New/candy shop/login.js
@@ -0,0 +1,45 @@
+let userName;
+let passwordInput;
+let messageText;
+let setPasswordButton;
+
+onload = function (event) {
+    userNameInput = document.getElementById('userNameInput');
+    passwordInput = document.getElementById('passwordInput');
+    messageText = document.getElementById('msg');
+    console.log(messageText);
+    setPasswordButton = document.getElementById('passwordButton');
+}
+
+function onPasswordChanged() {
+    const password = passwordInput.value;
+    let valid = false;
+    let message = '';
+  
+    if (password === '') {
+        message = '';
+    } else if (password.length < 5) {
+        message = 'Das Kennwort muss mindestens 5 Zeichen lang sein';
+        //Regex - regular expression 
+    } else if (password.search(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\W\S_]{5,}$/)) {
+        message = 'Das Kennwort muss mindestens jeweils einen Klein- und Großbuchstaben,sowie eine Zahl enthalten.';
+    } else {
+        message = 'Kennwort ist gültig';
+        valid = true;
+    }
+
+    if (valid) {
+        messageText.style.color = 'green';
+        setPasswordButton.disabled = false;
+    } else {
+        messageText.style.color = 'red';
+        setPasswordButton.disabled = true;
+    }
+    messageText.innerText = message;
+}
+
+function onButtonClick() {
+    alert('Kennwort gespeichert');
+    messageText.innerText = '';
+    passwordInput.value = '';
+}
